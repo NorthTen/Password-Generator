19ecb9ac09778090b57e4ae440148c11d0e8fdfe
RubyKyo <45924887+RubyKyo@users.noreply.github.com>
2023-05-08 19:27:56 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/bash
 mkdir -p out # Make directory out if it does not exist, if it does, continue.
-nimble build --opt:size --app:console --panics:on --mm:orc --backend:c --embedsrc:off --noNimblePath --panics:on -d:release --os:$1  # We pass a option to os. Default: "linux"
-mv -f ./passwordgen ./out # the app is compiled within ./ and so we must move it to ./out for organization.
+args="--opt:size --app:console --panics:on --mm:orc --backend:c --embedsrc:off --noNimblePath --panics:on -d:release --outdir:/out"
+if [$1 -eq ""]
+then
+    echo "no argument provided, defaulting to os: linux."
+    platform="linux"
+else
+    platform=$1
+fi
+
+if [ $platform == "all" ]
+then
+    echo "compiling for every platform."
+    ./build.sh "linux" && ./build.sh "windows"
+fi
+
+if [ $platform == "linux" ]
+then
+    nimble build $args --os:linux
+    mv -f ./passwordgen ./out
+elif [ $platform == "windows" ]
+then
+    nimble build $args -d:mingw
+    mv -f ./passwordgen.exe ./out
+# TODO: create build options for MacOS, and Android. Nim is cross-platform, it's just building the binaries all on linux is the problem.
+fi
